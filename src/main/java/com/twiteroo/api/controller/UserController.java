c3281cbc207bd692f04877c0d657cb3d49ffcd64
anarehder <anapaula.rehder@gmail.com>
2023-10-30 20:42:27 -0300

    feat: user controller and user dto

---
+++ src/main/java/com/twiteroo/api/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.twiteroo.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.twiteroo.api.dto.UserDTO;
+
+@RestController
+@RequestMapping("/ola")
+public class UserController {
+
+    @GetMapping
+    public String olaSpring() {
+        return "Olá teste!!";
+    }
+
+    @PostMapping
+    public void create(@RequestBody UserDTO req) {
+        System.out.println(req.avatar());
+    }
+}
---
+++ src/main/java/com/twiteroo/api/dto/UserDTO.java
@@ -0,0 +1,5 @@
+package com.twiteroo.api.dto;
+
+public record UserDTO(String name, String avatar) {
+    
+}
